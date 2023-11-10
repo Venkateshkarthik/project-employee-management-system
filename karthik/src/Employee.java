678b44bd26bea793648cf445ca99ee415d719dda
thiru <thiru@venkateshkarthi>
2023-11-10 17:58:29 +0530

    "Employee project"

---
+++ karthik/src/Employee.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class Employee {
+      public static void main(String[]args)throws Exception 
+      {
+    	   employee1();
+      }
+      public static void employee1()throws Exception
+      {
+    	  String url ="jdbc:mysql://localhost:3307/dsky";
+    	  String username="root";
+    	  String password="1999";
+    	  String Query="insert into dsky.trun values(?,?,?)";
+    	  Connection con = DriverManager.getConnection(url, username, password);
+     	  PreparedStatement pst =con.prepareStatement(Query);
+     	  pst.setInt(1,101);
+     	  pst.setString(2,"raghu");
+     	  pst.setInt(3,1000);
+     	  pst.executeUpdate();
+  		  con.close();
+    	  
+      }
+}
